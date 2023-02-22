1538e679cd499de426bb2de6aa55c373682d8802
Sagar Subedi <sagarxic@gmail.com>
2023-02-23 01:44:09 +0545

    Add post screen added, storage_methods.dart added

--- lib/screens/add_post_screen.dart
+++ lib/screens/add_post_screen.dart
@@ -1,15 +1,202 @@
-import 'package:flutter/material.dart';
+import 'dart:typed_data';
 
-class AddPostScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:instagram_clone/user_provider/user_provider.dart';
+import 'package:instagram_clone/utils/colors.dart';
+import 'package:provider/provider.dart';
+import 'package:instagram_clone/models/user.dart';
+
+import '../resources/firestore_methods.dart';
+import '../utils/utils.dart';
+
+class AddPostScreen extends StatefulWidget {
   const AddPostScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: IconButton(
-        onPressed: () {},
-        icon: Icon(Icons.upload),
-      ),
+  State<AddPostScreen> createState() => _AddPostScreenState();
+}
+
+class _AddPostScreenState extends State<AddPostScreen> {
+  bool isLoading = false;
+  Uint8List? _file;
+  final TextEditingController _descriptionController = TextEditingController();
+
+  void postImage(String uid, String username, String profImage) async {
+    setState(() {
+      isLoading = true;
+    });
+    // start the loading
+    try {
+      // upload to storage and db
+      String res = await FireStoreMethods().uploadPost(
+        _descriptionController.text,
+        _file!,
+        uid,
+        username,
+        profImage,
+      );
+      if (res == "success") {
+        setState(() {
+          isLoading = false;
+        });
+        showSnackBar(
+          'Posted!',
+          context,
+        );
+        clearImage();
+      } else {
+        showSnackBar(res, context);
+      }
+    } catch (err) {
+      setState(() {
+        isLoading = false;
+      });
+      showSnackBar(
+        err.toString(),
+        context,
+      );
+    }
+  }
+
+  void clearImage() {
+    setState(() {
+      _file = null;
+    });
+  }
+
+  _selectImage(BuildContext parentContext) async {
+    return showDialog(
+      context: parentContext,
+      builder: (BuildContext context) {
+        return SimpleDialog(
+          title: const Text('Create a Post'),
+          children: <Widget>[
+            SimpleDialogOption(
+                padding: const EdgeInsets.all(20),
+                child: const Text('Take a photo'),
+                onPressed: () async {
+                  Navigator.pop(context);
+                  Uint8List file = await pickImage(ImageSource.camera);
+                  setState(() {
+                    _file = file;
+                  });
+                }),
+            SimpleDialogOption(
+                padding: const EdgeInsets.all(20),
+                child: const Text('Choose from Gallery'),
+                onPressed: () async {
+                  Navigator.of(context).pop();
+                  Uint8List file = await pickImage(ImageSource.gallery);
+                  setState(() {
+                    _file = file;
+                  });
+                }),
+            SimpleDialogOption(
+              padding: const EdgeInsets.all(20),
+              child: const Text("Cancel"),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            )
+          ],
+        );
+      },
     );
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _descriptionController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final User user = Provider.of<UserProvider>(context).getUser;
+    return _file == null
+        ? Center(
+            child: IconButton(
+              onPressed: () {
+                _selectImage(context);
+              },
+              icon: Icon(Icons.upload),
+            ),
+          )
+        : Scaffold(
+            appBar: AppBar(
+              backgroundColor: mobileBackgroundColor,
+              leading: IconButton(
+                onPressed: clearImage,
+                icon: Icon(Icons.arrow_back),
+              ),
+              title: Text('Post to'),
+              centerTitle: false,
+              actions: [
+                TextButton(
+                  onPressed:  () => postImage(
+                    user.uid,
+                    user.username,
+                    user.photoUrl,
+                  ),
+                  child: Text(
+                    'Post',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                      color: Colors.blueAccent,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            body: Column(
+              children: <Widget>[
+                isLoading
+                    ? const LinearProgressIndicator()
+                    : const Padding(padding: EdgeInsets.only(top: 0.0)),
+                const Divider(),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    CircleAvatar(
+                      backgroundImage: NetworkImage(
+                        user.photoUrl!,
+                      ),
+                    ),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.45,
+                      child: TextField(
+                        controller: _descriptionController,
+                        decoration: const InputDecoration(
+                            hintText: "Write a caption...",
+                            border: InputBorder.none),
+                        maxLines: 8,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 45.0,
+                      width: 45.0,
+                      child: AspectRatio(
+                        aspectRatio: 487 / 451,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              fit: BoxFit.fill,
+                              alignment: FractionalOffset.topCenter,
+                              image: MemoryImage(_file!),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const Divider(),
+              ],
+            ),
+          );
   }
 }
--- lib/user_provider/user_provider.dart
+++ lib/user_provider/user_provider.dart
@@ -1,6 +1,6 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:instagram_clone/resources/auth_methods.dart';
+import 'package:instagram_clone/models/user.dart';
 
 class UserProvider with ChangeNotifier{
   User? _user;
@@ -8,7 +8,7 @@ class UserProvider with ChangeNotifier{
   User get getUser => _user!;
 
   Future<void> refreshUser()  async {
-    User user = (await _authMethods.getUserDetails()) as User;
+    User user = (await _authMethods.getUserDetails());
     _user = user;
     notifyListeners();
 }
